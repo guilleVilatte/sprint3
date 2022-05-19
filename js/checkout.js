4cc2b7595610d300ae6ec8e662d84e94ee94fe6b
Guille <guillevilatte@gmail.com>
2022-05-19 18:39:32 +0200

    nivel1y2faltaValidacionyEliminar

--- js/checkout.js
+++ js/checkout.js
@@ -1,14 +1,25 @@
 // Get the input fields
-var password = document.querySelector(".password");
-var phone = document.querySelector(".phone");
-var name = document.querySelector(".name");
+var password = document.getElementById("fPassword");
+var nombre = document.getElementById("fName");
+var phone = document.getElementById("fPhone");
+let email = document.getElementById("fEmail");
 
 // Get the error elements
 var errorPassword = document.getElementById("errorPassword");
 var errorName = document.getElementById("errorName");
 var errorPhone = document.getElementById("errorPhone");
+let errorEmail = document.getElementById("errorEmail");
 
 // Exercise 6
 function validate() {
   // Validate fields entered by the user: name, phone, password, and email
-}
+  e.preventDefault();
+  let isValid = false;
+  let nom = /^[A-Z]+$/i
+  let contraseña = /^(?=[A-Za-z]+[0-9]|[0-9]+[A-Za-z])[A-Za-z0-9]{4,8}$/;
+  //if (nombre.value.lenght<3 || ) {
+    
+  } 
+//}
+
+
--- js/shop.js
+++ js/shop.js
@@ -60,32 +60,80 @@ var cartList = [];
 
 // Improved version of cartList. Cart is an array of products (objects), but each one has a quantity field to define its quantity, so these products are not repeated.
 var cart = [];
+let eliminado;
+let seleccionado;
 
 var total = 0;
 
+let productsCopia = JSON.parse(JSON.stringify(products));
 // Exercise 1
 function buy(id) {
   // 1. Loop for to the array products to get the item to add to cart
   // 2. Add found product to the cartList array
+  seleccionado = productsCopia.find(product => product.id == id);
+  cartList.push(seleccionado);
+  generateCart();
+  applyPromotionsCart();
+  calculateTotal();
+  printCart();
 }
 
 // Exercise 2
-function cleanCart() {}
+function cleanCart() {
+  productsCopia = JSON.parse(JSON.stringify(products));
+  cartList = [];
+  cart = [];
+  total = 0;
+  document.querySelector(".listTitle").innerHTML = "Select Something";
+  document.querySelector(".list").innerHTML = ``;
+  document.querySelector(".clean").style.display = "none";
+  document.getElementById("count_product").innerHTML = `0`;
+}
 
 // Exercise 3
 function calculateTotal() {
   // Calculate total price of the cart using the "cartList" array
+  total = 0;
+  for (element of cart) {
+    if (element.subtotalWithDiscount == undefined) {
+      total += element.subtotal;
+    } else {
+      total += element.subtotalWithDiscount;
+    }
+  }
+  console.log(total);
 }
 
 // Exercise 4
 function generateCart() {
   // Using the "cartlist" array that contains all the items in the shopping cart,
   // generate the "cart" array that does not contain repeated items, instead each item of this array "cart" shows the quantity of product.
+  let valor;
+  for (seleccionado of cartList) {
+    valor = cart.indexOf(seleccionado);
+  }
+  if (valor == -1) {
+    seleccionado.quantity = 1;
+    seleccionado.subtotal = seleccionado.price;
+    cart.push(seleccionado);
+  } else {
+    seleccionado.quantity++;
+    seleccionado.subtotal += seleccionado.price;
+  }
 }
 
 // Exercise 5
 function applyPromotionsCart() {
   // Apply promotions to each item in the array "cart"
+  for (seleccionado of cart) {
+    if (seleccionado.id == 1 && seleccionado.quantity >= 3) {
+      console.log(seleccionado.quantity)
+      seleccionado.subtotalWithDiscount = 10 * seleccionado.quantity;
+    }
+    if (seleccionado.id == 3 && seleccionado.quantity >= 10) {
+      seleccionado.subtotalWithDiscount = (2 * seleccionado.subtotal) / 3;
+    }
+  }
 }
 
 // ** Nivell II **
@@ -95,17 +143,71 @@ function addToCart(id) {
   // Refactor previous code in order to simplify it
   // 1. Loop for to the array products to get the item to add to cart
   // 2. Add found product to the cart array or update its quantity in case it has been added previously.
+  seleccionado = productsCopia.find(product => product.id == id);
+  let valor = cart.indexOf(seleccionado);
+  if (valor == -1) {
+    seleccionado.quantity = 1;
+    seleccionado.subtotal = seleccionado.price;
+    cart.push(seleccionado);
+  } else {
+    seleccionado.quantity++;
+    seleccionado.subtotal += seleccionado.price;
+  }
+  applyPromotionsCart();
+  calculateTotal();
+  printCart();
 }
 
 // Exercise 8
 function removeFromCart(id) {
-  // 1. Loop for to the array products to get the item to add to cart
-  // 2. Add found product to the cartList array
+  if (seleccionado.quantity > 1) {
+    seleccionado.quantity--;
+    subtotal-=seleccionado.price;
+  } else {
+    eliminado = cart.filter((seleccionado) => seleccionado.id == id);
+    seleccionado.subtotal = 0;
+    console.log(eliminado)
+  }
 }
 
 // Exercise 9
 function printCart() {
   // Fill the shopping cart modal manipulating the shopping cart dom
+  document.getElementById("count_product").innerHTML = `${cart.length}`;
+  document.querySelector(".clean").style.display = "block";
+  document.querySelector(".listTitle").innerHTML = "Shopping Cart";
+  document.querySelector(".list").innerHTML = `<table class="table mt-3 me-3">
+                                                <thead>
+                                                  <tr>
+                                                    <th scope="col">Products</th>
+                                                    <th scope="col">Price</th>
+                                                    <th scope="col">Qty.</th>
+                                                    <th scope="col">Subtotal</th>
+                                                  </tr>
+                                                </thead>
+                                                <tbody>
+                                                </tbody>
+                                              </table>
+                                              <div class="text-center fs-2">Total:€${total}</div>`;
+  for (seleccionado of cart) {
+    if (seleccionado.subtotalWithDiscount == undefined) {
+      document.querySelector("tbody").insertAdjacentHTML("beforeend", `<tr>
+              <td>${seleccionado.name}</td>
+              <td>${seleccionado.price}</td>
+              <td>${seleccionado.quantity}</td>
+              <td>${seleccionado.subtotal}</td>
+              <td><a onclick="removeFromCart(id)" class="btn btn-danger pt-0 pb-0" role="button">-</a></td>
+            <tr>`)
+    } else {
+      document.querySelector("tbody").insertAdjacentHTML("beforeend", `<tr>
+            <td>${seleccionado.name}</td>
+            <td>${seleccionado.price}</td>
+            <td>${seleccionado.quantity}</td>
+            <td>${seleccionado.subtotalWithDiscount}</td>
+            <td><a onclick="removeFromCart(id)" class="btn btn-danger pt-0 pb-0" role="button">-</a></td>
+          <tr>`)
+    }
+  }
 }
 
 function open_modal() {
